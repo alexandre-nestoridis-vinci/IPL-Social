c7a3e3b7b0ba31c36633924d660d0b398ad5a26e
nestoridis <alexandre.nestoridis@student.vinci.be>
2025-01-17 16:30:37 +0100

    Refactor: refactoring of passwordChecker.spec / remove of the true condition

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,9 +1,7 @@
 function passwordChecker(password) {
     if (typeof password !== 'string') return false;
     if (password.length < 8) return false;
-
     return true;
 }
-
 
 module.exports = passwordChecker;
--- spec/passwordChecker.spec.js
+++ spec/passwordChecker.spec.js
@@ -3,14 +3,15 @@ const passwordChecker = require('../passwordChecker');
 
 describe('passwordChecker', () => {
 
-    // testé la longueur du mot de passe
+    // Test de la longueur du mot de passe
+
+        // quand il est en dessous de 8 caractères
     it('should return false if password is less than 8 characters', () => {
         expect(passwordChecker('1234567')).toBe(false);
         expect(passwordChecker('abc!1')).toBe(false);
     });
 
-    it('should return true if password is more or equal to 8 characters', () => {
-        expect(passwordChecker('12345678')).toBe(true);
-        expect(passwordChecker('abc!12345678')).toBe(true);
-    });
+
+  
+ 
 });
