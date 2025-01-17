de24d135329b96e41e11e407faf21c4457c233f6
nestoridis <alexandre.nestoridis@student.vinci.be>
2025-01-17 16:33:02 +0100

    Updated passwordChecker.js to include the validation of special characters

--- passwordChecker.js
+++ passwordChecker.js
@@ -1,6 +1,8 @@
 function passwordChecker(password) {
     if (typeof password !== 'string') return false;
     if (password.length < 8) return false;
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) return false; // Vérifie la présence de caractères spéciaux
+
     return true;
 }
 
