673da56631456543f30bc77ec570fffe8917b914
nestoridis <alexandre.nestoridis@student.vinci.be>
2025-01-17 16:35:03 +0100

    Updated passwordChecker.js to include the validation of the presence of numbers

--- passwordChecker.js
+++ passwordChecker.js
@@ -2,6 +2,8 @@ function passwordChecker(password) {
     if (typeof password !== 'string') return false;
     if (password.length < 8) return false;
     if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) return false; 
+    if (!/[0-9]/.test(password)) return false;
+    
     return true;
 }
 
