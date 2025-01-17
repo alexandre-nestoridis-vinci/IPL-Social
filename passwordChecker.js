95dcf0f24ca1f47a4ab18c5f18df9ff2980dca4d
nestoridis <alexandre.nestoridis@student.vinci.be>
2025-01-17 16:48:05 +0100

    Updated passwordChecker.js to include the validation of the non-presence of "IPL"

--- passwordChecker.js
+++ passwordChecker.js
@@ -3,6 +3,7 @@ function passwordChecker(password) {
     if (password.length < 8) return false;
     if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) return false; 
     if (!/[0-9]/.test(password)) return false;
+    if (/ipl/i.test(password)) return false; 
 
 
     return true;
